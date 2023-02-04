4ac5356114fc541206810be6e9639e9b5f7a5185
PEREGU <jcperegumma@gmail.com>
2023-02-04 12:38:09 -0500

    Tercero resuelto

--- checkpoint/03.js
+++ checkpoint/03.js
@@ -22,8 +22,28 @@
 //
 // ACLARACIÓN: Chequear el output de los test, para poder colocar los mensajes como corresponden!
 
+// 1.- Qué nos llega? Un arreglo con prendas de ropa que se solicitan. En una segunda invocación
+// se indican las que están en stock.
+// 2.- Qué se debe hacer? Retornar un mensaje que indique la cantidad de prendas disponibles acorde al stock
+// Retornar "No se encontraron las prendas solicitadas" si no se encontró disponibilidad
+// Retornar "Todas las prendas fueron encontradas" si las prendas solicitadas se encontraron
+// 3.- Cómo proceder?
+
 function chequearDisponibilidad(ropas) {
   // Tu código aquí:
+  var count = 0;
+  return function (stock) {
+    for (let i=0;i<ropas.length;i++) {
+      for (let j=0;j<stock.length;j++){
+        if (ropas[i] === stock[j]) {
+          count = count + 1;
+        }
+      }
+    }
+    if (count === 0) return "No se encontraron las prendas solicitadas";
+    if (count === ropas.length) return "Todas las prendas fueron encontradas";
+    return count.toString() + " de las " + ropas.length.toString() + " prendas solicitadas se encuentran en el stock"
+  }
 }
 
 // ⚠️ NO MODIFICAR NADA POR DEBAJO DE ESTA LÍNEA ⚠️
